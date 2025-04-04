feb3386e2d8e0bc360d762611b8b0f2508e958f3
Mascof17 <maxdebussac@gmail.com>
2025-04-04 15:50:25 +0200

    ajout de la nouvelle loi de commande à Annie

--- libraries/AP_Motors/AP_Motors6DOF.cpp
+++ libraries/AP_Motors/AP_Motors6DOF.cpp
@@ -175,16 +175,28 @@ void AP_Motors6DOF::setup_motors(motor_frame_class frame_class, motor_frame_type
         add_motor_raw_6dof(AP_MOTORS_MOT_6,     -1.0f,          0,              0,              -1.0f,              0,                  0,              6);
         break;
 
+    // case SUB_FRAME_CUSTOM:
+    //     _frame_class_string = "CUSTOM";
+    //     add_motor_raw_6dof(AP_MOTORS_MOT_1,     -1.0f,          0,              1.0f,          	-1.0f,    	    1.0f,              1.0f,           1);
+    //     add_motor_raw_6dof(AP_MOTORS_MOT_2,     0,              1.0f,           1.0f,          	1.0f,               1.0f,              1.0f,           2);
+    //     add_motor_raw_6dof(AP_MOTORS_MOT_3,     -1.0f,          1.0f,           0,     		-1.0f,              -1.0f,             1.0f,           3);
+    //     add_motor_raw_6dof(AP_MOTORS_MOT_4,     0,              0,              0,           	1.0f,               -1.0f,             1.0f,           4);
+    //     add_motor_raw_6dof(AP_MOTORS_MOT_5,      0,           	1.0f,           1.0f,           -1.0f,              -1.0f,             -1.0f,          5);
+    //     add_motor_raw_6dof(AP_MOTORS_MOT_6,     -1.0f,          0,              1.0f,           1.0f,               -1.0f,             -1.0f,          6);
+    //     add_motor_raw_6dof(AP_MOTORS_MOT_7,     0,          	0,              0,              -1.0f,              1.0f,              -1.0f,          7);
+    //     add_motor_raw_6dof(AP_MOTORS_MOT_8,    -1.0f,          1.0f,           0,              1.0f,               1.0f,              -1.0f,          8);
+    //     break;
+
     case SUB_FRAME_CUSTOM:
-        _frame_class_string = "CUSTOM";
-        add_motor_raw_6dof(AP_MOTORS_MOT_1,     -1.0f,          0,              1.0f,          	-1.0f,    	    1.0f,              1.0f,           1);
-        add_motor_raw_6dof(AP_MOTORS_MOT_2,     0,              1.0f,           1.0f,          	1.0f,               1.0f,              1.0f,           2);
-        add_motor_raw_6dof(AP_MOTORS_MOT_3,     -1.0f,          1.0f,           0,     		-1.0f,              -1.0f,             1.0f,           3);
-        add_motor_raw_6dof(AP_MOTORS_MOT_4,     0,              0,              0,           	1.0f,               -1.0f,             1.0f,           4);
-        add_motor_raw_6dof(AP_MOTORS_MOT_5,     0,           	1.0f,           1.0f,           -1.0f,              -1.0f,             -1.0f,          5);
-        add_motor_raw_6dof(AP_MOTORS_MOT_6,     -1.0f,          0,              1.0f,           1.0f,               -1.0f,             -1.0f,          6);
-        add_motor_raw_6dof(AP_MOTORS_MOT_7,     0,          	0,              0,              -1.0f,              1.0f,              -1.0f,          7);
-        add_motor_raw_6dof(AP_MOTORS_MOT_8,     -1.0f,          1.0f,           0,              1.0f,               1.0f,              -1.0f,          8);
+        _frame_class_string = "Custom_test_6DOF";
+        add_motor_raw_6dof(AP_MOTORS_MOT_1,    -(std::sqrt(2)/std::sqrt(3))f,  (std::sqrt(2)/std::sqrt(3))f,   -(std::sqrt(2)/std::sqrt(3))f,     -(std::sqrt(2)/std::sqrt(3))f,    0,    (std::sqrt(2)/std::sqrt(3))f, 1);
+        add_motor_raw_6dof(AP_MOTORS_MOT_2,    -(std::sqrt(2)/std::sqrt(3))f,  (std::sqrt(2)/std::sqrt(3))f,    (std::sqrt(2)/std::sqrt(3))f,   -(std::sqrt(2)/std::sqrt(3))f,   0,    -(std::sqrt(2)/std::sqrt(3))f,  2);
+        add_motor_raw_6dof(AP_MOTORS_MOT_3,    -1.0f,    -1.0f,    1.0f,   -1.0f,     -1.0f,   -1.0f     3);
+        add_motor_raw_6dof(AP_MOTORS_MOT_4,-    -1.0f,   -1.0f,      -1.0f,  -1.0f,     1.0f,   1.0f      4); 
+        add_motor_raw_6dof(AP_MOTORS_MOT_5,    1.0f,   -1.0f,     -1.0f,     -1.0f,    -1.0f,   1.0f,     5);
+        add_motor_raw_6dof(AP_MOTORS_MOT_6,-    1.0f,    -1.0f,     1.0f,  -1.0f,      1.0f,  -1.0f     6);
+        add_motor_raw_6dof(AP_MOTORS_MOT_7,    (std::sqrt(2)/std::sqrt(3))f,   (std::sqrt(2)/std::sqrt(3))f,   (std::sqrt(2)/std::sqrt(3))f,    -(std::sqrt(2)/std::sqrt(3))f,    0,    -(std::sqrt(2)/std::sqrt(3))f,  7);
+        add_motor_raw_6dof(AP_MOTORS_MOT_8,    (std::sqrt(2)/std::sqrt(3))f,  (std::sqrt(2)/std::sqrt(3))f,  -(std::sqrt(2)/std::sqrt(3))f,    -(std::sqrt(2)/std::sqrt(3))f,    0,   (std::sqrt(2)/std::sqrt(3))f,    8);
         break;
 
     case SUB_FRAME_SIMPLEROV_3:
